21be6d780f5b476f77c987136a039084c092de1e
yasuo3o3 <fujiwara@netservice.ne.jp>
2026-02-25 16:00:40 +0900

    ショートカット機能改善

--- background.js
+++ background.js
@@ -3,12 +3,6 @@ chrome.action.onClicked.addListener((tab) => {
     injectAndExecute(tab.id);
 });
 
-// キーボードショートカット（Ctrl+Shift+Eなど）を押したときのイベントリスナー
-chrome.commands.onCommand.addListener((command, tab) => {
-    if (command === "pick-color") {
-        injectAndExecute(tab.id);
-    }
-});
 
 function injectAndExecute(tabId) {
     // targetタブに content.js を注入・実行する
